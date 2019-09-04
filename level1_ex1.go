884101532e5b8ca9cc61633791b9428f6e7e3ede
zeeshanur rahman <zeeshanur.delhi@gmail.com>
2019-09-04 15:44:06 +0530

    level1 ex-1

--- level1_ex1.go
+++ level1_ex1.go
@@ -1,11 +1,14 @@
 package main
 
-import ("fmt")
+import "fmt"
 
-func main(){
-	var x := 42
-	var y := "James Bond"
-	var z := true
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
 
-	fmt.Println(x,y,z)
+	fmt.Println(x, y, z)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
 }
